0d7a30ebc76e33ddc6e0a024f534d495680c7e05
Connor <Woodconnor13@gmail.com>
2025-02-01 15:06:29 -0500

    Fibonacci Number

---
+++ leetcode2025/Problems/Easy/FibonacciNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode2025.Problems.Easy
+{
+    public static class FibonacciNumber
+    {
+        public static int Fib(int n)
+        {
+            if (n == 0) return 0;
+
+            int[] data = new int[n + 1];
+            data[0] = 0;
+            data[1] = 1;
+
+            for (int interval = 2; interval <= n; interval++)
+            {
+                data[interval] = data[interval - 1] + data[interval - 2];
+            }
+
+            return data[n];
+        }
+    }
+}
--- leetcode2025/Program.cs
+++ leetcode2025/Program.cs
@@ -1,6 +1,6 @@
 ﻿using leetcode2025.Problems.Easy;
 using leetcode2025.Problems.Medium;
 
-var i = ClimbingStairs.ClimbStairs(45);
+var i = FibonacciNumber.Fib(0);
 
 Console.WriteLine(i);
